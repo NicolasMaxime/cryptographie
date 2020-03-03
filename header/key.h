2357ef63de77eba9f53c7ca0b5db89a09b736c92
NicolasMaxime <maxime1.nicolas@gmail.com>
2020-03-03 16:56:02 +0100

    mise en forme du depot

--- header/key.h
+++ header/key.h
@@ -1,1 +1,6 @@
+#ifndef KEY_H_
+#define KEY_H_
+
 void key(mpz_t q, mpz_t p, mpz_t g, mpz_t Kp, mpz_t Ks);
+
+#endif /* !KEY_H_ */
--- src/main.c
+++ src/main.c
@@ -6,11 +6,40 @@
 #include "test.h"
 #include "keygen.h"
 
+void test(){
+  mpz_t res;
+  mpz_t res2;
+  mpz_t p;
+  mpz_t q;
+  mpz_t a;
+  mpz_t k;
+  
+  mpz_init(p);
+  mpz_init(k);
+  mpz_init(q);
+  mpz_init(a);
+  mpz_init(res);
+  mpz_init(res2);
+  mpz_set_ui(p, 71);
+  mpz_set_ui(q, 7);
+  mpz_set_ui(k, 10);
+  for (int i = 1; i != 70; i++){
+    mpz_set_ui(a, i);
+    puissance_mod(res, a, q, p); // a^q mod p
+    if (mpz_cmp_ui(res, 1) == 0){
+      puissance_mod(res2, a, k, p);
+      if (mpz_cmp_ui(res2, 1) != 0)
+	gmp_printf("%Zd ^ 7 % 71 = %Zd\n", a, res);
+    }
+  }
+}
+
 int main(){
   key_gpq_t *key;
 
   //key = keygen(256, 1792);
-  test_exponentiation_binaire(); //ok
+  test();
+  //test_exponentiation_binaire(); //ok
   //free(key);
   return 0;
 }
