ca05fd35dd13fab653ae9d3ebecce8abbde19717
IpseizAngela <31900083+IpseizAngela@users.noreply.github.com>
2020-03-03 17:07:52 +0100

    fonction key()

--- key.c
+++ key.c
@@ -8,6 +8,7 @@
 **  Key(); 
 **
 */
+static unsigned long int seed = 1;
 
 void key(mpz_t q, mpz_t p, mpz_t g, mpz_t Kp, mpz_t Ks) {
     //Cacul d'un nombre aléatoire x entre 2 et p-2 (Ks)
@@ -15,7 +16,12 @@ void key(mpz_t q, mpz_t p, mpz_t g, mpz_t Kp, mpz_t Ks) {
     mpz_init(x);
     mpz_t tmp;
     mpz_init(tmp);
+    
     gmp_randstate_t state;
+    gmp_randinit_mt(state);
+    gmp_randseed_ui(state, seed);
+    seed += 1;
+    
     mpz_sub_ui(tmp, p, 4);	// tmp = p - 4
     mpz_urandomm(x, state, tmp);// 0 <= g <= p - 4
     mpz_add_ui(x, x, 2);	// 2 <= g <= p - 2
@@ -26,8 +32,7 @@ void key(mpz_t q, mpz_t p, mpz_t g, mpz_t Kp, mpz_t Ks) {
     //Calcul de h = g^x(mod p) (Kp)
     mpz_t h;
     mpz_init(h);
-    mpz_pow(tmp, g, x);
-    mpz_mod(h, tmp, p);
+    mpz_powm(h, g, x, p);
     
     mpz_init(Kp);
     mpz_set(Kp, h);
